c9861b1592e9edf581d67d9ae7065a77af518f5b
davidrodier <david_rodier@hotmail.com>
2015-05-04 10:29:05 -0400

    Started

---
+++ Labo2/Labo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Labo2.Startup))]
+namespace Labo2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
