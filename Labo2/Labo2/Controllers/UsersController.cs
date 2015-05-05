69ee4371a61e29a1f91f95216907782755a6bc4e
davidrodier <david_rodier@hotmail.com>
2015-05-05 19:12:30 -0400

    zxc

--- Labo2/Labo2/Controllers/UsersController.cs
+++ Labo2/Labo2/Controllers/UsersController.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Labo2.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace Labo2.Controllers
 {
     public class UsersController : Controller
@@ -14,9 +15,49 @@ namespace Labo2.Controllers
         {
             return View();
         }
+        [HttpGet]
         public ActionResult Inscription()
         {
-            return View();
+            return View(new User());
+        }
+        [HttpPost]
+        public ActionResult Subscribe(User newUser)
+        {
+            User users = new User(Session["MainDB"]);
+            if (!String.IsNullOrEmpty(newUser.Prenom))
+            {
+                if (!String.IsNullOrEmpty(newUser.Nom))
+                {
+                    users.Prenom = newUser.Prenom;
+                    users.Nom = newUser.Nom;
+                    users.Telephone = newUser.Telephone;
+                    users.Code_Postal = newUser.Code_Postal;
+                    users.Naissance = newUser.Naissance;
+                    users.Sexe = newUser.Sexe;
+                    users.Etat_Civil = newUser.Etat_Civil;
+
+                    if (Request.Files.Count > 0)
+                    {
+                        var file = Request.Files[0];
+                        if (file != null && file.ContentLength > 0)
+                        {
+                            users.Picture = Guid.NewGuid().ToString();
+                            file.SaveAs(Server.MapPath(@"~\Avatars\") + users.Picture + ".png");
+                        }
+                    }
+                    users.Insert();
+                    return RedirectToAction("Index", "Home"); ;
+                }
+                else
+                {
+                    TempData["Notice"] = "Le mot de passe est vide...";
+                }
+            }
+            else
+            {
+                TempData["Notice"] = "Cet usager existe déjà...";
+            }
+            return View(newUser);
         }
 	}
 }
---
+++ Labo2/Labo2/Models/User.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Labo2.Models
+{
+    public class User : Labo2.Class.SqlExpressWrapper
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Prenom { get; set; }
+        [Required]
+        public string Nom { get; set; }
+        public string Telephone { get; set; }
+        public string Code_Postal { get; set; }
+        public DateTime Naissance { get; set; }
+        [Required]
+        public int Sexe { get; set; }
+        [Required]
+        public int Etat_Civil { get; set; }
+        [Required]
+        public string Picture { get; set; }
+
+        public User(Object connexionString)
+            : base(connexionString)
+        {
+            SQLTableName = "USERS";
+        }
+
+        public User()
+            : base("")
+        {
+        }
+        public override void GetValues()
+        {
+            Id = int.Parse(this["ID"]);
+            Prenom = this["PRENOM"];
+            Nom = this["NOM"];
+            Telephone = this["TELEPHONE"];
+            Code_Postal = this["CODE_POSTAL"];
+            Naissance = DateTime.Parse(this["NAISSANCE"]);
+            Sexe = int.Parse(this["SEXE"]);
+            Etat_Civil = int.Parse(this["ETAT_CIVIL"]);
+            Picture = this["Picture"];
+        }
+
+        public String GetAvatarURL()
+        {
+            String url;
+            if (String.IsNullOrEmpty(Picture))
+            {
+                url = @"Images/anonymous.jpg";
+            }
+            else
+            {
+                url = @"Avatars/" + Picture + ".png";
+            }
+
+            return url;
+        }
+        public override void Insert()
+        {
+            InsertRecord(Prenom, Nom, Telephone, Code_Postal, Naissance, Sexe, Etat_Civil, Picture);
+        }
+        public override void Update()
+        {
+            UpdateRecord(Id, Prenom, Nom, Telephone, Code_Postal, Naissance, Sexe, Etat_Civil, Picture);
+        }
+    }
+}
